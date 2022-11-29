b568413d8d8e91abba306cac1f62a6a51de8e40b
welijahclark <97975452+welijahclark@users.noreply.github.com>
2022-11-29 16:04:43 -0500

    Add files via upload

---
+++ FollowUp_NumericVersusInteger.R
@@ -0,0 +1,36 @@
+#Follow-Up to Question From Elijah
+
+#First, this demonstrates storing a floating-point number numerically versus as a series of integers
+#X is numeric, y is integer based
+X <- c(3.45, 8.69, 4.20, 6.91, 7.73)
+y <- c(3.45L, 8.69L, 4.20L, 6.91L, 7.73L)
+
+##I got the below trying to run y, but you can run y to see what happens on your computer
+#Warning messages:
+#1: integer literal 3.45L contains decimal; using numeric value 
+#2: integer literal 8.69L contains decimal; using numeric value 
+#3: integer literal 4.20L contains decimal; using numeric value 
+#4: integer literal 6.91L contains decimal; using numeric value 
+#5: integer literal 7.73L contains decimal; using numeric value 
+
+class(y)
+#This will show you what y got stored as in practice; I got the following;
+#[1] "numeric"
+
+#We CAN, however, brute-force assign a numeric vector as an integer variable...but it will round!
+#Below is how I did that, as well as printing the rounded vector
+Ecks = as.integer(X)
+print(Ecks)
+#[1] 3 8 4 6 7
+
+#Incidentally, these will result in different means, so that may have consequences for analysis.
+mean(X)
+#[1] 6.196
+mean(Ecks)
+#[1] 5.6
+
+#However, there is no major functional difference between numerical and integer data
+#(Provided that you don't need decimals for your numeric data, that is...)
+#We can demonstrate this with an object named Z, which is similar to Ecks other than being numeric
+Z<- c(3, 8, 4, 6, 7)
+mean(Z)
---
+++ FollowUp_PrintFunctionBrackets.R
@@ -0,0 +1,29 @@
+#A follow-up script to answer a question from Elijah
+
+#The [X] output from the print() function in R just denotes what line your output is on
+
+
+#With a relatively short vector, it will condense onto one line and not do a whole lot
+#You can run the below; my next comment is what I got
+SomeAnimalsILike <- c("cats", "dogs", "elk")
+print(SomeAnimalsILike)
+#[1] "cats" "dogs" "elk"
+
+#If I want to make a longer list with longer entries, it will print on separate numbered lines
+#The next series of comments are my results, and you can run this to see what you get
+ALongerListOfAnimalsILike <- c("cats", "dogs", "elk", "birds", "axolotls", "swordfish", "bears", "alligator snapping turtles (they're real animals I swear!)")
+print(ALongerListOfAnimalsILike)
+#[1] "cats"                                                      
+#[2] "dogs"                                                      
+#[3] "elk"                                                       
+#[4] "birds"                                                     
+#[5] "axolotls"                                                  
+#[6] "swordfish"                                                 
+#[7] "bears"                                                     
+#[8] "alligator snapping turtles (they're real animals I swear!)"
+
+#Fun fact; there is a function called cat() that does NOT print these numbers, but is otherwise the same
+#My guess is that it's called "cat" because of conCATenation
+cat(ALongerListOfAnimalsILike)
+#The next line is what I got;
+#cats dogs elk birds axolotls swordfish bears alligator snapping turtles (they're real animals I swear!)
