1cc347cead31e9e383c4a5861cacbbf89987476e
welijahclark <97975452+welijahclark@users.noreply.github.com>
2022-11-29 16:00:53 -0500

    Update Master Workshop Exercise Script.R

--- Master Workshop Exercise Script.R
+++ Master Workshop Exercise Script.R
@@ -1,4 +1,5 @@
-#Workshop Exercise 1
+##Workshop Exercise 1
+#We're doing four-function arithmetic where X=20 and y=4, then printing the results.
 x <- 20 
 y <- 4
 adding <- (x+y)
@@ -10,13 +11,17 @@ print(multipling)
 dividing <- x/y
 print(dividing)
 
-#Workshop Exercise 2
+##Workshop Exercise 2
+#Given information that there were 15444 Starbucks locations across 50 US states and 7 US territories, manually find the average
+#Note: the number of Starbucks locations in 2021 can be found at https://www.statista.com/statistics/218360/number-of-starbucks-stores-in-the-us/
 StarbucksStores <- 15444
 USStatesTerritories <- 57
 AverageStores <- StarbucksStores/USStatesTerritories
 print(AverageStores)
 
 #Workshop Exercise 3
+#Making objects for Calories, Milk Types, and Caffeine, and making sure that they're in the correct type for later analysis
+#Note: Data used was a cleaned Starbucks Menu dataset that is available in this repository.
 Calories <- starbucks_drinkMenu_expanded_cleaned$Calories
 MilkType <- starbucks_drinkMenu_expanded_cleaned$MilkType_Char
 Caffeine <- starbucks_drinkMenu_expanded_cleaned$`Caffeine (mg)`
@@ -33,6 +38,7 @@ mean(MoreMoreCaffeine)
 median(MoreMoreCaffeine)
 
 #Workshop Exercise 4
+#An ANOVA demonstration using the built-in dataset "ToothGrowth", comparing supplement type to final tooth length
 print(ToothGrowth)
 Length <- ToothGrowth$len
 Supplement <- ToothGrowth$supp
@@ -40,6 +46,7 @@ ToothANOVA <- aov(Length ~ Supplement)
 summary(ToothANOVA)
 
 #Workshop Exercise 5
+#A simple-linear-regression demonstration using the built-in dataset "trees", comparing tree girth to tree height
 print(trees)
 Girth2 <- trees$Girth
 Height2 <- trees$Height
@@ -47,15 +54,20 @@ TreeRegression <- lm(Height2 ~ Girth2)
 summary(TreeRegression)
 
 #Package Installation Demo
+#Note: I demonstrated with beepr, but also had ggplot2 as the real exercise. Uninstalling beepr before workshops to re-install it is a must here.
 install.packages("beepr")
 library(beepr)
 install.packages("ggplot2")
 library(ggplot2)
 
 ###ggplot2 demo
-#ANOVA
+#ANOVA graph demonstration
+
+#No additions (black and white)
 ggplot(ToothGrowth, aes(Supplement, Length)) + geom_boxplot()
+#Adding yellow and red
 ggplot(ToothGrowth, aes(Supplement, Length)) + geom_boxplot(fill="yellow", color="red")
+#Note: This one is to demonstrate that you can also use hexadecimal values; this one is FSU Gold.
 ggplot(ToothGrowth, aes(Supplement, Length)) + geom_boxplot(fill="CEB888", color="red")
 
 #LinReg
@@ -64,14 +76,14 @@ ggplot(trees, aes(Height2, Girth2)) + geom_point() + geom_smooth(method="lm")
 
 
 ####Final Exercise!
-#Regression
+#Regression of caffeine to calories from Starbucks Data (basically, is there a correlation of calories to caffeine. (No.))
 CaffeineRegression <- lm(Calories ~ MoreCaffeine)
 summary(CaffeineRegression)
 
-#ANOVA
+#ANOVA of type of Milk to calories
 MilkANOVA <- aov(Calories ~ MilkType)
 summary(MilkANOVA)
 
-#Our two Plots
+#Plots for both our ANOVA and our linear regression models.
 ggplot(starbucks_drinkMenu_expanded_cleaned, aes(MilkType, Calories))+geom_boxplot(fill="green")
 ggplot(starbucks_drinkMenu_expanded_cleaned, aes(MoreCaffeine, Calories)) +geom_point()+geom_smooth(method="lm")
